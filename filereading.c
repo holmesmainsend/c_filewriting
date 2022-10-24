a4be699d07ea87d45684378bd219ea0e16cc5dd7
Mona Lisa <holmesmainsend@gmail.com>
2022-10-24 15:39:25 -0400

    finished basic line reading code

--- filereading.c
+++ filereading.c
@@ -2,9 +2,20 @@
 #include <stdlib.h>
 
 int main() {
-    FILE *fPointer = fopen("employees.txt", "a");
+    char line1[255];
+    char line2[255];
+    char line3[255];
+    char line4[255];
+    FILE *fPointer = fopen("employees.txt", "r");
 
-    
+    fgets(line1, 255, fPointer);
+    printf("%s\n", line1);
+    fgets(line2, 255, fPointer);
+    printf("%s\n", line2);
+    fgets(line3, 255, fPointer);
+    printf("%s\n", line3);
+    fgets(line4, 255, fPointer);
+    printf("%s\n", line4);
 
     fclose(fPointer);
     return 0;
