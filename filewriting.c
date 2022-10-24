9b52344d3d5aff5259b0e84806d5e2b4eed4e304
Mona Lisa <holmesmainsend@gmail.com>
2022-10-24 15:23:40 -0400

    successfully wrote employee info to new file

--- filewriting.c
+++ filewriting.c
@@ -4,7 +4,7 @@
 int main() {
     FILE *fPointer = fopen("employees.txt", "w");
 
-    
+    fprintf(fPointer, "Jeff, Receptionist\nMarta, Clerk\nBill, Cashier\n");
 
     fclose(fPointer);
     return 0;
